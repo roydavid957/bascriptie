f1a16d1e51ccb1cecb0661034afde64a4e62bb0f
roydavid957 <r.a.david@student.rug.nl>
2017-11-23 14:56:45 +0100

    Update get_data.py
    changed pre-processing bit + punctlist

--- get_data.py
+++ get_data.py
@@ -1,200 +1,126 @@
-#Roy David
-#Bascriptie
-
-#Descr:	collects and creates datafile used for bathesis
-#Usage:	python get_data.py
-
-import json
-from TwitterSearch import TwitterUserOrder, TwitterSearch, TwitterSearchException
-from twitter import OAuth
-import langid
-import string
-import time
 import numpy as np
-import random
-import time
-import sys
+import ast
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, classification_report, confusion_matrix
+from sklearn.model_selection import StratifiedKFold
+from nltk import ngrams
+from nltk.corpus import stopwords
 
-start_time = time.time()
+f = open("news.filtered","r").readlines()
 
-#if len(sys.argv) == 2:
+x_train = []
+x2_train = []
+x3_train = []
+y_train = []
+n = 2
+c = 3
+for line in f:
+	line = line.strip("\n")
+	line = ast.literal_eval(line)
+	line = [line]
+	for l in line:
+		d = {}
+		twograms = []
+		two_grams = []
+		d2 = {}
+		trigrams = []
+		tri_grams = []
+		d3 = {}
+		for k,v in l.items():
+			y_train.append(l[k]["location"])
+			for tweet in l[k]["text"]:
+				tweet = [word for word in tweet.split() if word not in stopwords.words('dutch')]
+				for word in tweet:
+					d[word] = 1
+				twograms.append(ngrams(tweet, n))
+				for char in twograms:
+					for grams in char:
+						two_grams.append(grams[0]+"_"+grams[1])
+				for grams in two_grams:
+					d2[grams] = 1
+				trigrams.append(ngrams(tweet, c))
+				for char in trigrams:
+					for grams in char:
+						tri_grams.append(grams[0]+"_"+grams[1]+"_"+grams[2])
+				for grams in tri_grams:
+					d3[grams] = 1
+		x_train.append(d)
+		x2_train.append(d2)
+		x3_train.append(d3)
+
+y_train_np = np.array(y_train)
+
+#data = x_train
+#print("len data: ",len(data))
+label_train = y_train_np
+#print("len label_train: ",len(label_train))
+#data_dev = x_train[500:800]
+#print("len data_dev: ",len(data_dev))
+#label_dev = y_train_np[500:800]
+#print("len label_dev: ",len(label_dev))
+#print("")
+
+vectorizer = DictVectorizer()
+classifier = LogisticRegression()
+
+x_list = [x_train,x2_train,x3_train]
+for x in x_list:
+	if x == x_train:
+		print("\t#####\tUNIGRAMS\t#####\n")
+	if x == x2_train:
+		print("\t#####\tBIGRAMS\t#####\n")
+	if x == x3_train:
+		print("\t#####\tTRIGRAMS\t#####\n")
+	X_train = vectorizer.fit_transform(x, label_train)
+	#X_dev = vectorizer.transform(data_dev)
+
+	"""KFOLD"""
+	print("KFold, n=10:")
+	kfold = StratifiedKFold(n_splits=10, shuffle=True)
+	acc_score = 0
+	acc_list = []
+	for train_index, test_index in kfold.split(X_train, label_train):
+		#print("TRAIN:", train_index, "TEST:", test_index)
+		X_trn, X_tst = X_train[train_index], X_train[test_index]
+		y_trn, y_tst = label_train[train_index], label_train[test_index]
+		classifier.fit(X_trn, y_trn)
+		predicted_y = classifier.predict(X_tst)
+		#print("predicted_y: ",len(predicted_y),"\n",predicted_y)
+		#print("")
+		acc = accuracy_score(y_tst, predicted_y)
+		#print("accuracy_score:\n",acc)
+		acc_score += float(acc)
+		acc_list.append(float(acc))
+		print("classification_report:\n",classification_report(y_tst, predicted_y))
+		print("")
+		print(classifier.classes_)
+		print(confusion_matrix(y_tst, predicted_y, labels=["E", "N", "S", "W"]))
+		print("")
+	print("\tRESULTS:")
+	print("avg accuracy_score: ",(acc_score / (len(acc_list))))
+	print("acc_list: ",len(acc_list),"\n\t",acc_list)
+	print("")
+	"""KFOLD"""
+
+	"""MOST INF FEAT"""
+	#def show_most_informative_features(vectorizer, classifier, n=20):
+	print("Most informative features:")
+	n=20
+	feature_names = vectorizer.get_feature_names()
+	coefs_with_fns = sorted(zip(classifier.coef_[0], feature_names))
+	top = zip(coefs_with_fns[:n], coefs_with_fns[:-(n + 1):-1])
+	for (coef_1, fn_1), (coef_2, fn_2) in top:
+		print("\t%.4f\t%-15s\t\t%.4f\t%-15s" % (coef_1, fn_1, coef_2, fn_2))
+	print("")
+	"""MOST INF FEAT"""
 	
-print("Descr:	collects and creates datafile used for bathesis")
-print("Usage:	python get_data.py")
-
-cred = json.load(open("credentials-roy.json"))
-my_authentication = OAuth(cred["ACCESS_TOKEN"], cred["ACCESS_TOKEN_SECRET"], \
-			  cred["CONSUMER_KEY"], cred["CONSUMER_SECRET"])
-
-ts = TwitterSearch(access_token=cred["ACCESS_TOKEN"],access_token_secret=cred["ACCESS_TOKEN_SECRET"],
-			   consumer_key=cred["CONSUMER_KEY"], consumer_secret=cred["CONSUMER_SECRET"])
-
-regions = ["north","east","west","south"]
-news_list = []
-random100_list = []
-
-punctuation_list = ["!","(",")","`","~","[","{","]","}","\\","|",";",":","'",'"',",","<",".",">","/","?"] # all punctuation except "#","@","^","$","%","&","*","-","_","=","+"
-
-filt1 = False
-filt2 = True
-for region in regions:
-
-	#print("\n\t",sys.argv[-1])
-	print("\n\t",region)
-	print("collecting data might take a while...")
-
-	#infile = "data-master/uniq_users_{}.txt".format(sys.argv[-1])
-	infile = "data-master/uniq_users_{}.txt".format(region)
-
-	uniq_users_region = open(infile).readlines()
-	users_region = []
-	for line in uniq_users_region:
-		line = line.split("\t")
-		users_region.append(line[0])
-
-	np.random.seed(1234)
-	np.random.shuffle(users_region)
-
-	region_list = []
-
-	for user in users_region:
-		if len(region_list) != 250:
-		#if len(region_list) != 25:
-			### filter retweets, the tweets from emoticons \n, @'s, punctuation and urls and lowercase text ###
-
-			
-			try:
-				twitter_user_order = TwitterUserOrder(user)
-
-				all_tweets = []
-
-				for tweet in ts.search_tweets_iterable(twitter_user_order):
-					if len(all_tweets) != 100:
-					#if len(all_tweets) != 10:
-						if "belgië" not in tweet['user']['location'].lower() and "belgium" not in tweet['user']['location'].lower() and "belgie" not in tweet['user']['location'].lower():
-							if "RT" != tweet['text'].split()[0]: # filter retweets by skipping them
-								lang = langid.classify(tweet['text'])
-
-								if lang[0] == "nl": # check wether language is Dutch
-									
-									if filt1 == True:
-										if "é" in tweet['text'] or "á" in tweet['text'] or "ó" in tweet['text'] or "ú" in tweet['text'] or "í" in tweet['text']: # change é etc to e etc to keep word after encoding
-											for word in tweet['text'].split():
-												for letter in word:
-													if letter == "é":
-														tweet['text'] = tweet['text'].replace(letter,"e")
-													if letter == "á":
-														tweet['text'] = tweet['text'].replace(letter,"a")
-													if letter == "ó":
-														tweet['text'] = tweet['text'].replace(letter,"o")
-													if letter == "ú":
-														tweet['text'] = tweet['text'].replace(letter,"u")
-													if letter == "í":
-														tweet['text'] = tweet['text'].replace(letter,"i")
-
-										tweet['text'] = tweet['text'].encode('ascii','ignore').decode("utf-8").lower() # filter emoticons like "\ud*" etc
-									
-									if filt2 == True:
-										tweet['text'] = tweet['text'].lower()
-										if "\n" in tweet['text']: # replace every \n in tweet with space
-											for word in tweet['text'].split():
-												tweet['text'] = tweet['text'].replace("\n"," ")
-							
-										if "\r" in tweet['text']: # replace every \r in tweet with space
-											for word in tweet['text'].split():
-												tweet['text'] = tweet['text'].replace("\r"," ")
-
-										if "http" in tweet['text']: # remove url from tweet (by replacing with nothing)
-											for word in tweet['text'].split():
-												if "http" in word:
-													tweet['text'] = tweet['text'].replace(word,"")
-										
-										for word in tweet['text'].split():
-											for char in word:
-												if char in punctuation_list:
-													tweet['text'] = tweet['text'].replace(char," ")
-									
-									if filt1 == True:
-										if "http" in tweet['text']: # remove url from tweet (by replacing with nothing)
-											for word in tweet['text'].split():
-												if "http" in word:
-													tweet['text'] = tweet['text'].replace(word,"")
-
-										if "@" in tweet['text']: # remove "@" from tweet (by replacing with nothing)
-											for word in tweet['text'].split():
-												if "@" in word:
-													tweet['text'] = tweet['text'].replace(word,"")
-
-										for item in punctuation_list: # remove all punctuation except "#"
-											if item in tweet['text']:
-												tweet['text'] = tweet['text'].replace(item," ")
-
-									if tweet['text'] != "" and tweet['text'].split() != []:
-										all_tweets.append(tweet)
-
-					elif len(all_tweets) == 100:
-					#elif len(all_tweets) == 10:
-						for tweet in all_tweets:
-							init = False
-							d = {}
-							d[tweet['user']['screen_name']] = {'text': [tweet['text']], 'location': region[0].upper()}
-							for dic in region_list:
-								if tweet['user']['screen_name'] in dic:
-									dic[tweet['user']['screen_name']]['text'].append(tweet['text'])
-									init = True
-							if init == False:
-								region_list.append(d)
-							#region_list.append(tweet['user']['screen_name']+"\t"+tweet['text']+"\t"+tweet['user']['location'])
-						break
-
-
-			except TwitterSearchException as e: # catch errors
-				print(e)
-				#pass
-	
-		elif len(region_list) == 250:
-		#elif len(region_list) == 25:
-			#np.random.seed(1234)
-			#np.random.shuffle(region_list)
-			print("len region_list: ",len(region_list))
-			with open(region,"w") as outfile:
-				for line in region_list:
-					outfile.write(str(line))
-					outfile.write("\n")
-			for line in region_list:
-				news_list.append(line)
-			print("len news: ",len(news_list))
-			np.random.seed(1234)
-			random25nrs = []
-			while len(random25nrs) != 25:
-				randomint = np.random.randint(0, 250)
-				#randomint = np.random.randint(0, 25)
-				if randomint not in random25nrs:
-					random25nrs.append(randomint)
-			print("len random25nrs: ",len(random25nrs))
-			for nr in random25nrs:
-				random100_list.append(region_list[nr])
-			print("len random100: ",len(random100_list))
-			break
-	if len(random100_list) == 100:
-	#if len(random100_list) == 8:
-		with open("random100","w") as random100file:
-			for line in random100_list:
-				random100file.write(str(line))
-				random100file.write("\n")
-	if len(news_list) == 1000:
-	#if len(news_list) == 100:
-		np.random.seed(1234)
-		np.random.shuffle(news_list)
-		with open("news","w") as newsfile:
-			for line in news_list:
-				newsfile.write(str(line))
-				newsfile.write("\n")
-
-print("collecting data done...")
-
-#else:
-#	print("Something went wrong, please try again...")
-#	print("Usage:	python get_user_tweets.py north/east/west/south")
-
-print("\n--- %s seconds ---" % (time.time() - start_time))
+	#classifier.fit(X_train, label_train)
+	#print("classifier:\n",classifier)
+	#print("")
+	#predicted_y = classifier.predict(X_dev)
+	#print("predicted_y: ",len(predicted_y),"\n",predicted_y)
+	#print("")
+	#print("accuracy_score:\n",accuracy_score(label_dev, predicted_y))
+	#print("classification_report:\n",classification_report(label_dev, predicted_y))
+print("done...")
