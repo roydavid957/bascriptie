d1a32db19d80975599c0d9e6889bd41a45f8046e
roydavid957 <r.a.david@student.rug.nl>
2017-12-01 10:05:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,43 @@
+#Roy David
+#Bascriptie
+#Geographical Home Location Prediction of a Dutch Twitter user
+
+#Descr:	code for the experiments for the bathesis
+#	using StratifiedKFold
+#	using the classifiers: LogisticRegression and LinearSVC
+#	outputs f1_score, classification_report, confusion_matrix
+#Usage:	python test.py
+
 import numpy as np
 import ast
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score, f1_score, classification_report, confusion_matrix
 from sklearn.model_selection import StratifiedKFold
 from nltk import ngrams
 from nltk.corpus import stopwords
+import time
 
-f = open("news.filtered","r").readlines()
+start_time = time.time()
+
+print("Descr:\tdoes the experiments for the bathesis")
+print("\tusing StratifiedKFold")
+print("\tusing the classifiers: LogisticRegression and LinearSVC")
+print("\tusing uni-, bi-, trigrams as features")
+print("\toutputs f1_score, classification_report, confusion_matrix")
+print("Usage:\tpython test.py")
+print("")
+
+f = open("news.uniq","r").readlines()
+
+print("creating X's and Y's, might take some time...")
+print("")
 
 x_train = []
 x2_train = []
 x3_train = []
+x12_train = []
 y_train = []
 n = 2
 c = 3
@@ -22,60 +46,53 @@ for line in f:
 	line = ast.literal_eval(line)
 	line = [line]
 	for l in line:
-		d = {}
+		d1 = {}
 		twograms = []
 		two_grams = []
 		d2 = {}
 		trigrams = []
 		tri_grams = []
 		d3 = {}
+		d12 = {}
 		for k,v in l.items():
 			y_train.append(l[k]["location"])
 			for tweet in l[k]["text"]:
 				tweet = [word for word in tweet.split() if word not in stopwords.words('dutch')]
-				for word in tweet:
-					d[word] = 1
+				for word in tweet: #create unigrams
+					d1[word] = 1 
+					d12[word] = 1
 				twograms.append(ngrams(tweet, n))
-				for char in twograms:
+				for char in twograms: #create bigrams
 					for grams in char:
 						two_grams.append(grams[0]+"_"+grams[1])
 				for grams in two_grams:
 					d2[grams] = 1
+					d12[grams] = 1
 				trigrams.append(ngrams(tweet, c))
-				for char in trigrams:
+				for char in trigrams: #create trigrams
 					for grams in char:
 						tri_grams.append(grams[0]+"_"+grams[1]+"_"+grams[2])
 				for grams in tri_grams:
 					d3[grams] = 1
-		x_train.append(d)
+		x_train.append(d1)
 		x2_train.append(d2)
 		x3_train.append(d3)
+		x12_train.append(d12)
 
 y_train_np = np.array(y_train)
 
-#data = x_train
-#print("len data: ",len(data))
 label_train = y_train_np
-#print("len label_train: ",len(label_train))
-#data_dev = x_train[500:800]
-#print("len data_dev: ",len(data_dev))
-#label_dev = y_train_np[500:800]
-#print("len label_dev: ",len(label_dev))
-#print("")
 
 vectorizer = DictVectorizer()
 
-classifiers = [LogisticRegression(),SVC(),GaussianNB]
-x_list = [x_train,x2_train,x3_train]
-all_avg_acc_score = []
-feat = False
+classifiers = [LogisticRegression(),LinearSVC()]
+x_list = [x_train,x2_train,x3_train,x12_train]
+top_f1_score = 0
 for classifier in classifiers:
 	if classifier == classifiers[0]:
 		print("\t#####\tLogisticRegression\t#####\n")
 	if classifier == classifiers[1]:
-		print("\t#####\tSVC\t#####\n")
-	if classifier == classifiers[2]:
-		print("\t#####\tGaussianNB\t#####\n")
+		print("\t#####\tLinearSVC\t#####\n")
 	for x in x_list:
 		if x == x_train:
 			print("\t#####\tUNIGRAMS\t#####\n")
@@ -83,66 +100,60 @@ for classifier in classifiers:
 			print("\t#####\tBIGRAMS\t#####\n")
 		if x == x3_train:
 			print("\t#####\tTRIGRAMS\t#####\n")
+		if x == x12_train:
+			print("\t#####\tUNI+BIGRAMS\t#####\n")
 		X_train = vectorizer.fit_transform(x, label_train)
-		#X_dev = vectorizer.transform(data_dev)
 
 		"""KFOLD"""
 		print("KFold, n=10:")
 		kfold = StratifiedKFold(n_splits=10, shuffle=True)
-		acc_score = 0
-		acc_list = []
+		f1score = 0
+		f1_list = []
 		for train_index, test_index in kfold.split(X_train, label_train):
-			#print("TRAIN:", train_index, "TEST:", test_index)
 			X_trn, X_tst = X_train[train_index], X_train[test_index]
 			y_trn, y_tst = label_train[train_index], label_train[test_index]
 			classifier.fit(X_trn, y_trn)
 			predicted_y = classifier.predict(X_tst)
 			#print("predicted_y: ",len(predicted_y),"\n",predicted_y)
 			#print("")
-			acc = accuracy_score(y_tst, predicted_y)
-			#print("accuracy_score:\n",acc)
-			acc_score += float(acc)
-			acc_list.append(float(acc))
-			if feat == True:
-				print("classification_report:\n",classification_report(y_tst, predicted_y))
-				print("")
-				print(classifier.classes_)
-				print(confusion_matrix(y_tst, predicted_y, labels=["E", "N", "S", "W"]))
-				print("")
-		all_avg_acc_score.append((acc_score / (len(acc_list))))
+			f1 = f1_score(y_tst, predicted_y, average='micro')
+			f1score += f1
+			f1_list.append(f1)
+			#print("classification_report:\n",classification_report(y_tst, predicted_y))
+			#print("")
+			#print(classifier.classes_)
+			#print(confusion_matrix(y_tst, predicted_y, labels=["E", "N", "S", "W"]))
+			#print("")
 		print("\tRESULTS:")
-		print("avg accuracy_score: ",(acc_score / (len(acc_list))))
-		print("acc_list: ",len(acc_list),"\n\t",acc_list)
+		avg_f1_score = f1score / len(f1_list)
+		print("avg f1_score: ",avg_f1_score)
+		print("f1_list: ",len(f1_list),"\n\t",f1_list)
 		print("")
 		"""KFOLD"""
 
 		"""MOST INF FEAT"""
-		if feat == True:
-			#def show_most_informative_features(vectorizer, classifier, n=20):
-			print("Most informative features:")
-			n=20
-			feature_names = vectorizer.get_feature_names()
-			coefs_with_fns = sorted(zip(classifier.coef_[0], feature_names))
+		#n=10
+		#feature_names = vectorizer.get_feature_names()
+		#print("feature_names")
+		#coefs_with_fns = sorted(zip(classifier.coef_[0], feature_names))
+		#top = zip(coefs_with_fns[:n], coefs_with_fns[:-(n + 1):-1])
+		#for (coef_1, fn_1), (coef_2, fn_2) in top:
+		#	print("\t%.4f\t%-15s\t\t%.4f\t%-15s" % (coef_1, fn_1, coef_2, fn_2))
+		#print("")
+		#if avg_f1_score > top_f1_score:
+		#	top_f1_score = avg_f1_score
+		print("Most informative features:")
+		n=10
+		feature_names = vectorizer.get_feature_names()
+		for i in range(0,len(classifier.coef_)):
+			coefs_with_fns = sorted(zip(classifier.coef_[i], feature_names))
 			top = zip(coefs_with_fns[:n], coefs_with_fns[:-(n + 1):-1])
+			print("i",i,classifier.classes_[i])
 			for (coef_1, fn_1), (coef_2, fn_2) in top:
 				print("\t%.4f\t%-15s\t\t%.4f\t%-15s" % (coef_1, fn_1, coef_2, fn_2))
-			print("")
+		print("")
 		"""MOST INF FEAT"""
-	
-		#classifier.fit(X_train, label_train)
-		#print("classifier:\n",classifier)
-		#print("")
-		#predicted_y = classifier.predict(X_dev)
-		#print("predicted_y: ",len(predicted_y),"\n",predicted_y)
-		#print("")
-		#print("accuracy_score:\n",accuracy_score(label_dev, predicted_y))
-		#print("classification_report:\n",classification_report(label_dev, predicted_y))
-print("\tAVERAGE ACC:")
-for score in all_avg_acc_score:
-	if score == all_avg_acc_score[0]:
-		print("LogiscticRegression: ", score)
-	if score == all_avg_acc_score[1]:
-		print("SVC: ",score)
-	if score == all_avg_acc_score[2]:
-		print("GuassianNB: ",score)
+
 print("done...")
+
+print("\n--- %s seconds ---" % (time.time() - start_time))
